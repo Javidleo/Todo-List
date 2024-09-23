3962da00b8bcfbabaff7d97dee0ebcf7ad4bd431
Javid Hassani <91068797+Javidleo@users.noreply.github.com>
2024-09-23 08:30:36 +0330

    adding new entity Task

---
+++ ToDoList.API/Domain/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.API.Domain;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreationTime { get; set; }
+}
